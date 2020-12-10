e1fd2fb5aba17f815e5afaf351ba3c542037805e
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-12-10 23:16:39 +0100

    added automatic subscribe and unsubscribe

--- Jerboa/src/Jerboa/Core/EventBus.h
+++ Jerboa/src/Jerboa/Core/EventBus.h
@@ -10,44 +10,15 @@
 namespace Jerboa {
     class EventObserverBase;
 
-    class HandlerFunctionBase {
-    public:
-        void exec(Event* evnt) {
-            call(evnt);
-        }
-    private:
-        virtual void call(Event* evnt) = 0;
-    };
-
-    template<class T, class EventType>
-    class MemberFunctionHandler : public HandlerFunctionBase
-    {
-    public:
-        typedef void (T::* MemberFunction)(EventType*);
-
-        MemberFunctionHandler(T* instance, MemberFunction memberFunction) : instance{ instance }, memberFunction{ memberFunction } {};
-
-        void call(Event* evnt) {
-            // Cast event to the correct type and call member function
-            (instance->*memberFunction)(static_cast<EventType*>(evnt));
-        }
-
-        // Pointer to class instance
-        T* instance;
-
-        // Pointer to member function
-        MemberFunction memberFunction;
-        
-    };
-
-    typedef std::list<HandlerFunctionBase*> HandlerList;
     class EventBus {
+        typedef std::function<void(Event&)> CallbackFunc;
+        typedef std::list<CallbackFunc*> CallbackFuncList;
     public:
         template<class EventType>
-        void Publish(EventType* evnt) {
+        void Publish(Event* evnt) {
             static_assert(std::is_base_of<Event, EventType>::value, "EventType must inherit from Event");
 
-            HandlerList* handlers = mSubscribers[typeid(EventType)];
+            CallbackFuncList* handlers = mSubscribers[typeid(EventType)];
 
             if (handlers == nullptr) {
                 return;
@@ -55,41 +26,38 @@ namespace Jerboa {
 
             for (auto& handler : *handlers) {
                 if (handler != nullptr) {
-                    handler->exec(evnt);
+                    (*handler)(*evnt);
                 }
             }
         }
 
     private:
-        std::unordered_map<std::type_index, HandlerList*> mSubscribers;
+        template<class EventType>
+        void Subscribe(CallbackFunc& callback) {
+            CallbackFuncList* handlers = mSubscribers[typeid(EventType)];
 
-        template<class T, class EventType>
-        void Subscribe(T* instance, void (T::* memberFunction)(EventType*)) {
-            static_assert(std::is_base_of<Event, EventType>::value, "EventType must inherit from Event");
-
-            HandlerList* handlers = mSubscribers[typeid(EventType)];
-
-            //First time initialization
             if (handlers == nullptr) {
-                handlers = new HandlerList();
+                handlers = new CallbackFuncList();
                 mSubscribers[typeid(EventType)] = handlers;
             }
 
-            handlers->push_back(new MemberFunctionHandler(instance, memberFunction));
+            handlers->push_back(&callback);
         }
 
-        template<class EventType, class T>
-        void Unsubscribe(T* instance) {
+        template<class EventType>
+        void Unsubscribe(CallbackFunc& callback) {
             static_assert(std::is_base_of<Event, EventType>::value, "EventType must inherit from Event");
 
-            HandlerList* handlers = mSubscribers[typeid(EventType)];
+            CallbackFuncList* handlers = mSubscribers[typeid(EventType)];
 
             if (handlers == nullptr) {
                 return;
             }
 
-            //handlers->remove_if([](MemberFunctionHandler<T, EventType> elem) { instance == elem.instance });
+            handlers->remove(&callback);
         }
+
+        std::unordered_map<std::type_index, CallbackFuncList*> mSubscribers;
 
         friend class Jerboa::EventObserverBase;
     };
@@ -99,14 +67,14 @@ namespace Jerboa {
         EventObserverBase(EventBus* eventBus) : mEventBus(eventBus) {}
 
     protected:
-        template<class T, class EventType>
-        void Subscribe(T* instance, void (T::* memberFunction)(EventType*)) {
-            mEventBus->Subscribe(instance, memberFunction);
+        template<class EventType>
+        void Subscribe(std::function<void(Event& evnt)>& callback) {
+            mEventBus->Subscribe<EventType>(callback);
         }
 
-        template<class T, class EventType>
-        void Unsubscribe(T* instance, void (T::* memberFunction)(EventType*)) {
-            mEventBus->Unsubscribe<EventType>(instance);
+        template<class EventType>
+        void Unsubscribe(std::function<void(Event& evnt)>& callback) {
+            mEventBus->Unsubscribe<EventType>(callback);
         }
 
     private:
@@ -117,7 +85,7 @@ namespace Jerboa {
     template<class T, class EventType>
     class EventObserver : EventObserverBase {
     public:
-        typedef void (T::* MemberFunction)(EventType*);
+        typedef void (T::* MemberFunction)(const EventType&);
 
         EventObserver(EventBus* eventBus): 
             EventObserverBase(eventBus)
@@ -126,22 +94,22 @@ namespace Jerboa {
         }
 
         ~EventObserver() {
-            if (mInstance) {
+            //if (mInstance) {
                 JERBOA_LOG_INFO("Unsubscribing");
-                Unsubscribe(mInstance, mMemberFunction);
-            }
+                Unsubscribe<EventType>(mCallback);
+            //}
             JERBOA_LOG_INFO("Destroying EventObserver");
         }
 
         void OnEvent(T* instance, MemberFunction memberFunction) {
+            static_assert(std::is_base_of<Event, EventType>::value, "EventType must inherit from Event");
+            mCallback = [=](Event& evnt) { (instance->*memberFunction)(static_cast<const EventType&>(evnt)); };
+            Subscribe<EventType>(mCallback);
+
             JERBOA_LOG_INFO("Subscribing");
-            mInstance = instance;
-            mMemberFunction = memberFunction;
-            Subscribe(instance, memberFunction);
         }
 
     private:
-        T* mInstance;
-        MemberFunction mMemberFunction;
+        std::function<void(Event& evnt)> mCallback;
     };
 };
--- Sandbox/src/App.cpp
+++ Sandbox/src/App.cpp
@@ -1,19 +1,22 @@
 #include "Jerboa.h"
 #include "Jerboa/EntryPoint.h"
 
-static class TestEvent : public Jerboa::Event {};
+static class TestEvent : public Jerboa::Event {
+public:
+	TestEvent(int value) : mValue(value) {}
+	int mValue;
+};
 //class TestEvent {};
 
 class TestSubscriber {
 public:
 	TestSubscriber(Jerboa::EventBus* eventBus, std::string message) : mMessage(message), mTestEventObserver(eventBus) {
-		//eventBus->Subscribe(this, &TestSubscriber::OnTestEvent);
 		mTestEventObserver.OnEvent(this, &TestSubscriber::OnTestEvent);
 	}
 
 private:
-	void OnTestEvent(TestEvent* evnt) {
-		JERBOA_LOG_INFO(mMessage);
+	void OnTestEvent(const TestEvent& evnt) {
+		JERBOA_LOG_ERROR("Message: {}  Value: {}", mMessage, evnt.mValue);
 	}
 
 	std::string mMessage;
@@ -32,11 +35,12 @@ public:
 		JERBOA_LOG_INFO("SandboxApp started");
 		
 		TestSubscriber s1(&mEventBus, "S1 received TestEvent");
-		//{
-			//TestSubscriber s2(&mEventBus, "S2 received TestEvent");
-		//}
+		{
+			TestSubscriber s2(&mEventBus, "S2 received TestEvent");
+			mEventBus.Publish<TestEvent>(new TestEvent(1));
+		}
 		
-		mEventBus.Publish(new TestEvent());
+		mEventBus.Publish<TestEvent>(new TestEvent(2));
 	}
 
 	~SandboxApp()
