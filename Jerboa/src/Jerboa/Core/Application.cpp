dd159bd18e4eb43f7df046dcdf599310a1a89835
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-12-16 22:07:30 +0100

    added LayerStack

--- Jerboa/src/Jerboa/Core/Application.cpp
+++ Jerboa/src/Jerboa/Core/Application.cpp
@@ -4,9 +4,19 @@
 namespace Jerboa {
     void Application::Run() {
         OnStart();
-        while (m_Running) {
+        while (mRunning) {
             // TODO
         }
         OnShutdown();
+    }
+
+    void Application::PushLayer(Layer* layer) {
+        mLayerStack.PushLayer(layer);
+        layer->OnAttach();
+    }
+
+    void Application::PushOverlay(Layer* overlay) {
+        mLayerStack.PushOverlay(overlay);
+        overlay->OnAttach();
     }
 }
